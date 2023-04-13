fa4385b5c4577e0a8b54c0fdba93514845dc4039
TheGatesScratch <mtkarels@outlook.com>
2023-04-13 10:40:52 +0200

    Simplify methods. Fix eventType changes.

--- src/main/java/io/github/thegatesdev/actionable/ReactorFactories.java
+++ src/main/java/io/github/thegatesdev/actionable/ReactorFactories.java
@@ -10,15 +10,14 @@ import io.github.thegatesdev.mapletree.data.Readable;
 import io.github.thegatesdev.mapletree.registry.Identifiable;
 import org.bukkit.event.Event;
 
+import javax.annotation.Nullable;
 import java.util.*;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 // Store all eventFactories. Allow multiple modification through factoriesOf.
 public class ReactorFactories implements Identifiable, DataTypeHolder<ReactorFactory<?>.ReadReactor> {
     private final Map<EventType<?>, ReactorFactory<?>> classMapped = new HashMap<>();
-    private final Map<EventType<?>, List<?>> factoriesOfCache = new HashMap<>();
+    private final Map<Class<? extends Event>, List<?>> factoriesOfCache = new HashMap<>();
     protected final EventTypes eventTypes;
 
     private final Readable<ReactorFactory<?>.ReadReactor> dataType;
@@ -37,11 +36,9 @@ public class ReactorFactories implements Identifiable, DataTypeHolder<ReactorFac
     // -- FACTORY GETTERS
 
     @SuppressWarnings("unchecked")
-    public final <E extends Event> ReactorFactory<E> getFactory(EventType<E> eventClass) {
-        if (eventClass == null || !EventTypes.hasHandlerList(eventClass)) return null;
-        final ReactorFactory<E> reactorFactory = (ReactorFactory<E>) classMapped.get(eventClass);
-        if (reactorFactory != null) return reactorFactory;
-        return (ReactorFactory<E>) classMapped.compute(eventClass, (a, c) -> new ReactorFactory<>(eventClass));
+    public final <E extends Event> ReactorFactory<E> getFactory(EventType<E> eventType) {
+        if (eventType == null) return null;
+        return (ReactorFactory<E>) classMapped.computeIfAbsent(eventType, (t) -> new ReactorFactory<>(eventType));
     }
 
     public final ReactorFactory<?> getFactory(String eventId) {
@@ -51,39 +48,29 @@ public class ReactorFactories implements Identifiable, DataTypeHolder<ReactorFac
     // -- MASS ACTION
 
     @SuppressWarnings("unchecked")
-    protected final <E extends Event> List<ReactorFactory<? extends E>> factoriesOf(EventType<E> baseEventClass) {
+    @Nullable
+    protected final <E extends Event> List<ReactorFactory<? extends E>> factoriesOf(Class<E> baseEventClass) {
         List<ReactorFactory<? extends E>> output;
         {
             output = (List<ReactorFactory<? extends E>>) factoriesOfCache.get(baseEventClass);
             if (output != null) return output;
+            else if (factoriesOfCache.containsKey(baseEventClass)) return null;
         }
-        final List<EventType<? extends E>> classes = eventTypes.listenableSubEvents(baseEventClass);
-        output = new ArrayList<>(classes.size());
-        for (EventType<?> clazz : classes)
-            output.add((ReactorFactory<? extends E>) getFactory(clazz));
+        final List<EventType<? extends E>> types = eventTypes.listenableEvents(baseEventClass);
+        if (types == null) {
+            factoriesOfCache.put(baseEventClass, null);
+            return null;
+        }
+        output = new ArrayList<>(types.size());
+        for (EventType<?> type : types)
+            output.add((ReactorFactory<? extends E>) getFactory(type));
         factoriesOfCache.put(baseEventClass, output);
         return output;
     }
 
-    public final <E extends Event> void subFactories(EventType<E> baseEventType, Consumer<ReactorFactory<? extends E>> consumer) {
-        final var eventFactories = factoriesOf(baseEventType);
-        if (eventFactories.isEmpty()) throw new RuntimeException("Warning: doWithFactoriesOf did nothing");
-        eventFactories.forEach(consumer);
-    }
-
-    public final <E extends Event> void subFactories(Class<E> baseEventClass, Consumer<ReactorFactory<? extends E>> consumer) {
-        subFactories(eventTypes.get(baseEventClass), consumer);
-    }
-
-
-    @SuppressWarnings("unchecked")
-    public final <E extends Event, D> void addPerformers(String name, EventType<E> baseEventType, Predicate<E> eventPredicate, Function<E, D> dataGetter, DataTypeHolder<Predicate<D>> conditionType, DataTypeHolder<Consumer<D>> actionType) {
-        for (final ReactorFactory<? extends E> factory : factoriesOf(baseEventType))
-            ((ReactorFactory<E>) factory).addPerformer(name, eventPredicate, conditionType, actionType, dataGetter);
-    }
-
-    public final <E extends Event, D> void addPerformers(String name, Class<E> baseEventClass, Predicate<E> eventPredicate, Function<E, D> dataGetter, DataTypeHolder<Predicate<D>> conditionType, DataTypeHolder<Consumer<D>> actionType) {
-        addPerformers(name, eventTypes.get(baseEventClass), eventPredicate, dataGetter, conditionType, actionType);
+    public final <E extends Event> void eachFactory(Class<E> baseEventType, Consumer<ReactorFactory<? extends E>> consumer) {
+        final var factoriesOf = factoriesOf(baseEventType);
+        if (factoriesOf != null) factoriesOf.forEach(consumer);
     }
 
 
